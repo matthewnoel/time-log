0a9cc1d66dc4d0412736f0f3c1a19a5a5f7644fd
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2023-08-02 20:12:39 -0700

    Revert "gh pages specific pwa fixes"
    This reverts commit f555dc151affca2375bd647229249b5834c3a7c0.

--- docs/sw.js
+++ docs/sw.js
@@ -1,55 +1,7 @@
-var GHPATH = '/time-tracker';
-var APP_PREFIX = 'evsects_';
-var VERSION = 'version_001';
-var URLS = [
-    `${GHPATH}/`,
-    `${GHPATH}/index.html`,
-    `${GHPATH}/global.css`,
-    `${GHPATH}/icons/icon-32.png`,
-    `${GHPATH}/icons/icon-512.png`,
-    `${GHPATH}/build/bundle.js`,
-    `${GHPATH}/build/bundle.js.map`,
-    `${GHPATH}/build/bundle.css`
-]
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+});
 
-var CACHE_NAME = APP_PREFIX + VERSION
-self.addEventListener('fetch', function (e) {
-    console.log('Fetch request : ' + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('Responding with cache : ' + e.request.url);
-                return request
-            } else {
-                console.log('File is not cached, fetching : ' + e.request.url);
-                return fetch(e.request)
-            }
-        })
-    )
-})
-
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('Installing cache : ' + CACHE_NAME);
-            return cache.addAll(URLS)
-        })
-    )
-})
-
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            var cacheWhitelist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX)
-            })
-            cacheWhitelist.push(CACHE_NAME);
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheWhitelist.indexOf(key) === -1) {
-                    console.log('Deleting cache : ' + keyList[i]);
-                    return caches.delete(keyList[i])
-                }
-            }))
-        })
-    )
-})
+self.addEventListener("fetch", (e) => {
+    return '🎾';
+});
